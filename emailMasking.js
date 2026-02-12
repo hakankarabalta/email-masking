7f0fed4020a4afacb6b8ce51e4e46d0c0a06de28
hakankarabalta <info@hakankarabalta.com>
2026-02-12 20:23:01 +0300

    update

--- emailMasking.js
+++ emailMasking.js
@@ -2,7 +2,7 @@ const email = document.getElementById('email');
 const form = document.getElementById('maskForm');
 
 
-
+// email mask function
 function maskEmail(inputData){
   const [name, domain] = inputData.split('@');
   return name.substring(0, 1) + "*".repeat(Math.max(1, name.length - 1)) + '@' + domain;
@@ -13,7 +13,7 @@ function maskEmail(inputData){
 
 
 
-
+// submit and result
 maskForm.addEventListener('submit',
   function(event) {
     
@@ -27,13 +27,15 @@ maskForm.addEventListener('submit',
     maskForm.innerHTML = `
     <h2>işlem Başarılı</h2>
      <p>Maskelenmiş Veri: <strong>${maskedResult}</strong></p>
-            <button id="copyBtn">Kopyala</button>
+            <button id="copyBtn" >Kopyala</button>
             <button onclick="location.reload()">Yeni Sorgu</button>
             `;
 });
 
-const copyBtn = document.getElementById("copyBtn");
 
+
+
+//copy button
 maskForm.addEventListener("click", function (e) {
   if (e.target.id === "copyBtn") {
   const copy = maskForm.querySelector("strong").textContent;
