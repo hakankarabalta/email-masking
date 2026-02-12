90353e7242d4e388242493a44e5569243c684953
hakankarabalta <info@hakankarabalta.com>
2026-02-12 16:39:57 +0300

    update

--- emailMasking.js
+++ emailMasking.js
@@ -1,2 +1,32 @@
 const email = document.getElementById('email');
-const button = document.getElementById('button')
+const form = document.getElementById('maskForm');
+
+
+
+function maskEmail(inputData){
+  const [name, domain] = inputData.split('@');
+  return name.substring(0, 1) + "*".repeat(Math.max(1, name.length - 1)) + '@' + domain;
+  
+}
+
+
+
+
+
+
+maskForm.addEventListener('submit',
+  function(event) {
+    
+    event.preventDefault();
+    
+    
+    const emailValue = email.value;
+    
+    const maskedResult = maskEmail(emailValue);
+    
+    maskForm.innerHTML = `
+    <h2>işlem Başarılı</h2>
+     <p>Maskelenmiş Veri: <strong>${maskedResult}</strong></p>
+            <button onclick="location.reload()">Yeni Sorgu</button>
+            `;
+  });
